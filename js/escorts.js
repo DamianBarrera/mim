9899937af1a05cd270f6aad5989d8dc7620ed570
Damian Barreraclear <>
2022-04-12 00:08:24 -0300

    menu busqueda ahora se cierra cliqueando afuer

--- js/escorts.js
+++ js/escorts.js
@@ -1,7 +1,9 @@
+const body = document.querySelector('body');
 const lista = document.querySelector('.lista-chicas');
 const nombres = document.querySelectorAll('.galeria .card');
 const search = document.querySelector('.search');
  
+
 const allNames = [];
 nombres.forEach( N => {
     allNames.push(N.lastElementChild.textContent.toLowerCase())
@@ -10,15 +12,28 @@ nombres.forEach( N => {
 
 allNames.forEach( N => {
     const li = document.createElement('li');
+    li.classList.add('lista')
     li.innerHTML = `<a href="chicas/${N}.html">${N}</a>`;
     lista.appendChild(li)
 })
 
- 
-
- search.addEventListener('click', ()=> {
-    lista.classList.toggle('hide')
+  search.addEventListener('click', ()=> {
+    lista.classList.toggle('hide');          
 })
+
+body.addEventListener('click', ( e ) => {
+    if( !e.target.classList.contains('busqueda-nombre') ){
+        if(!e.target.classList.contains('fa-angle-down')){
+            if( !e.target.classList.contains('lista-chicas')){
+                if( !e.target.classList.contains('lista')) {
+                    lista.classList.add('hide')
+                }
+            }
+        }
+    }
+})
+
+ 
 
 
 

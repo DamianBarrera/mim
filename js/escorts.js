7dedbd8080cdb63e901c73cb352d4f3cb79062fa
Damian Barrera <>
2022-04-12 17:14:48 -0300

    ahora las imagenes se crean de forma dinamica

--- js/escorts.js
+++ js/escorts.js
@@ -1,40 +1,65 @@
 const body = document.querySelector('body');
 const lista = document.querySelector('.lista-chicas');
-const nombres = document.querySelectorAll('.galeria .card');
+const galeria = document.querySelector('.galeria');
 const search = document.querySelector('.search');
+
+
+const nombreChicas = [ "demi","rosiris","daniela","marisol","danielle","juliana","daniella chavez",
+"estephania","ana cheri","abigail","anastasiya","angie","becky","sophie","Jessica","riae"]
+
+// Inserta imagenes en la galeria 
+
+    nombreChicas.forEach( N => {
+    const div = document.createElement('div');
+    div.classList.add('card')
+
+    div.innerHTML = `<a href="../chicas/${N}.html" >
+                        <img src="../img/${N}/${N}1.jpg" alt="${N}" title="Encuentros Casuales | ${N}">
+                     </a>
+                      <p class="name">${N}</p>  `
+
+    galeria.appendChild(div)
+
+   
+ })
+
  
+ 
+// **********************************************
+// Inserta nombres en la seccion de busqueda
 
-const allNames = [];
-nombres.forEach( N => {
-    allNames.push(N.lastElementChild.textContent.toLowerCase())
-    allNames.sort();
-} )
+const nombres = document.querySelectorAll('.galeria .card');
 
-allNames.forEach( N => {
-    const li = document.createElement('li');
-    li.classList.add('lista')
-    li.innerHTML = `<a href="chicas/${N}.html">${N}</a>`;
-    lista.appendChild(li)
-})
+ const allNames = [];
+    nombres.forEach( N => {
+        allNames.push(N.lastElementChild.textContent.toLowerCase())
+        allNames.sort();
+    } )
 
-  search.addEventListener('click', ()=> {
-    lista.classList.toggle('hide');          
-})
+    allNames.forEach( N => {
+        const li = document.createElement('li');
+        li.classList.add('lista')
+        li.innerHTML = `<a href="chicas/${N}.html">${N}</a>`;
+        lista.appendChild(li)
+    })
 
-body.addEventListener('click', ( e ) => {
-    if( !e.target.classList.contains('busqueda-nombre') ){
-        if(!e.target.classList.contains('fa-angle-down')){
-            if( !e.target.classList.contains('lista-chicas')){
-                if( !e.target.classList.contains('lista')) {
-                    lista.classList.add('hide')
+    search.addEventListener('click', ()=> {
+        lista.classList.toggle('hide');          
+    })
+
+    body.addEventListener('click', ( e ) => {
+        if( !e.target.classList.contains('busqueda-nombre') ){
+            if(!e.target.classList.contains('fa-angle-down')){
+                if( !e.target.classList.contains('lista-chicas')){
+                    if( !e.target.classList.contains('lista')) {
+                        lista.classList.add('hide')
+                    }
                 }
             }
         }
-    }
-})
+    })
 
  
-
-
+ 
 
  
